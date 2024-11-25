cbe2155658b96d41c80e6cdbf1b8cff8ccfa6cfa
Braden Griebel <bgriebel@uw.edu>
2024-11-25 11:23:28 -0800

    Created pytorch VAE model for the flux values

--- metabolic_vae/scripts/01_sample_model.py
+++ metabolic_vae/scripts/01_sample_model.py
@@ -6,10 +6,12 @@ Generate Flux Samples from Un-constrained Model, and Split into Training, Valida
 # Imports
 # Standard Library Imports
 import pathlib
+from typing import cast
 
 # External Imports
 from cobra.sampling import OptGPSampler
 import metworkpy
+import pandas as pd
 from sklearn.model_selection import train_test_split
 
 # Local Imports
@@ -27,12 +29,15 @@ PROCESSES = 16
 NUM_SAMPLES = 100_000
 TEST_PROP = 0.2
 VALIDATION_PROP = 0.1
+THINNING = 500
+RETURN_FLUXES = False
+
 
 # Create Sampler Object
-sampler = OptGPSampler(model=BASE_MODEL, thinning=250, processes=PROCESSES)
+sampler = OptGPSampler(model=BASE_MODEL, thinning=THINNING, processes=PROCESSES)
 
 # Generate Samples
-samples = sampler.sample(NUM_SAMPLES)
+samples = sampler.sample(NUM_SAMPLES, fluxes=RETURN_FLUXES)
 
 # Filter for valid samples
 valid_samples = samples[sampler.validate(samples) == "v"]
@@ -45,7 +50,11 @@ train_samples, validate_samples = train_test_split(
     train_samples, test_size=VALIDATION_PROP / (1 - TEST_PROP), shuffle=True
 )  # split 10% of total off as validation samples
 
+train_samples = cast(pd.DataFrame, train_samples)
+test_samples = cast(pd.DataFrame, test_samples)
+validate_samples = cast(pd.DataFrame, validate_samples)
+
 # Save the samples
-train_samples.to_parquet(FLUX_SAMPLE_PATH / "train.parquet", index=False)
-validate_samples.to_parquet(FLUX_SAMPLE_PATH / "validate.parquet", index=False)
-test_samples.to_parquet(FLUX_SAMPLE_PATH / "test.parquet", index=False)
+train_samples.to_parquet(FLUX_SAMPLE_PATH / "train_fluxes.parquet", index=False)
+validate_samples.to_parquet(FLUX_SAMPLE_PATH / "validate_fluxes.parquet", index=False)
+test_samples.to_parquet(FLUX_SAMPLE_PATH / "test_fluxes.parquet", index=False)
---
+++ metabolic_vae/torch_models/metvae.py
@@ -0,0 +1,95 @@
+"""
+Defining a VAE Model Using Pytorch
+"""
+
+# Standard Librar Imports
+from typing import List
+
+# External Imports
+import torch
+import torch.nn as nn
+
+# Local Imports
+
+
+# VAE Class
+class MetabolicVAE(nn.Module):
+    def __init__(
+        self, input_dim: int, hidden_dims: List[int], latent_dim: int, device
+    ) -> None:
+        """
+        Create the VAE Object
+
+        Args:
+            input_dim: Input dimension, number of features of the dataset to encode
+            hidden_dims: List of sizes of the hidden dimensions
+            latent_dim: Size of the latent dimension
+            device: Torch device being used
+
+        Returns:
+            None
+        """
+        super(MetabolicVAE, self).__init__()
+        self.device = device
+
+        # Encoding
+        # Build up encoder with different hidden_dims
+        # Initial Layer
+        layers = [nn.Linear(input_dim, hidden_dims[0]), nn.LeakyReLU()]
+        # Build up the hidden Layers
+        lag_dim = hidden_dims[0]
+        for dim in hidden_dims[1:]:
+            layers += [nn.Linear(lag_dim, dim), nn.LeakyReLU()]
+            lag_dim = dim
+        # Build up to the encoder
+        self.encoder = nn.Sequential(*layers)
+        # Generate the Mean and Variance in the Latent Space
+        self.mean_layer = nn.Linear(hidden_dims[-1], latent_dim)
+        self.logvar_layer = nn.Linear(hidden_dims[-1], latent_dim)
+
+        # Create Decoder
+        layers = [nn.Linear(latent_dim, hidden_dims[-1]), nn.LeakyReLU()]
+        lag_dim = hidden_dims[-1]
+        for dim in hidden_dims[-2::-1]:
+            layers += [nn.Linear(lag_dim, dim), nn.LeakyReLU()]
+        layers += [
+            nn.Linear(hidden_dims[0], input_dim),
+            nn.ReLU(),
+        ]  # This is assuming all the fluxes are positive, so split forward and reverse rxns
+        self.decoder = nn.Sequential(*layers)
+
+    def encode(self, x):
+        """Encode x to the latent dim mean and logvar"""
+        x = self.encoder(x)
+        mean, logvar = self.mean_layer(x), self.logvar_layer(x)
+        return mean, logvar
+
+    def reparameterize(self, mean, logvar):
+        """Use reparameterization trick to generate a random sample from the mean and logvar"""
+        var = torch.exp(logvar)
+        eps = torch.randn_like(var)
+        return mean + var * eps
+
+    def decode(self, x):
+        """
+        Decode from the latent representation
+        """
+        return self.decoder(x)
+
+    def forward(self, x):
+        """Run the forward pass"""
+        mean, logvar = self.encode(x)
+        latent_sample = self.reparameterize(mean, logvar)
+        x_recon = self.decode(latent_sample)
+        return (
+            x_recon,
+            mean,
+            logvar,
+        )  # x_recon used for reconstruction error, mean/logvar for KL divergence term
+
+
+def loss_function(x, x_recon, mean, logvar):
+    """Calculate the Loss of the VAE"""
+    reproduction_loss = nn.functional.mse_loss(x_recon, x)
+    kl_loss = -0.5 * torch.sum((1 + logvar - mean.power(2) - torch.exp(logvar)))
+    return reproduction_loss + kl_loss
