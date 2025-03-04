eadc25a4a9ba2e4940ca3b870dac4e5698a8a665
Braden Griebel <bgriebel@uw.edu>
2025-03-04 10:58:13 -0800

    Added script in kinase target project for combining data from modeling, and protein properties predictions

--- kinase_target_ml/scripts/04_combine_data.py
+++ kinase_target_ml/scripts/04_combine_data.py
@@ -50,6 +50,26 @@ acetylation_targets = pd.read_excel(
     DATA_PATH / "xie_acetylation.xlsx", sheet_name="SWNU_Mtu_Kac", index_col=0, header=0
 )  # Index is Locus number
 
-# Join the model data and the protein info dataframes 
+# Join the model data and the protein info dataframes
 combined_df = pd.merge(model_data, protein_data, left_index=True, right_index=True)
-print(combined_df)
+
+# Add in target columns (PknB-PknL, and Acetylation)
+for kinase in kinase_list:
+    # Create the column with all False
+    combined_df[kinase] = False
+    # Set the correct genes to True
+    combined_df.loc[combined_df.index.isin(kinase_targets[kinase]), kinase] = True
+
+# Add in the total kinase target
+combined_df["phosphorylated"] = combined_df[kinase_list].any(axis=1)
+# Add in the acetylation target
+combined_df["acetylated"] = False
+combined_df.loc[combined_df.index.isin(acetylation_targets.index), "acetylated"] = True
+
+# Split the data into a features dataframe, and a targets dataframe
+target_df = combined_df[kinase_list + ["phosphorylated", "acetylated"]]
+feature_df = combined_df.drop(kinase_list + ["phosphorylated", "acetylated"], axis=1)
+
+# Save the feature and target DataFrames to the Results folder
+feature_df.to_csv(RESULTS_PATH / "feature_df.csv", index=True)
+target_df.to_csv(RESULTS_PATH / "target_df.csv", index=True)
